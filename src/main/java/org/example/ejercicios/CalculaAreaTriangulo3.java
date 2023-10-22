613ee9a86e5371b5afb11cea908c93834a41fb76
David <david.penagos1407@gmail.com>
2023-10-22 11:52:17 -0500

    Agregando cambios en el repositorio (David Julian Penagos)

--- src/main/java/org/example/ejercicios/CalculaAreaTriangulo3.java
+++ src/main/java/org/example/ejercicios/CalculaAreaTriangulo3.java
@@ -3,14 +3,27 @@ package org.example.ejercicios;
 public class CalculaAreaTriangulo3 {
 	public static void main(String[] args) {
 		Triangulo triangulo = new Triangulo("rojo", 5.0D, 4.0D);
-		double valor_area_triangulo = 0D;
-		double b = triangulo.b();
-		double h = triangulo.h();
-		valor_area_triangulo =  0.5 * b * h;
-		System.out.println("Altura del triángulo: " + triangulo.h());
-		System.out.println("Base del triángulo: " + triangulo.b());
-		System.out.println("Área del triángulo: " + valor_area_triangulo);
+		double areaTriangulo = calcularAreaTriangulo(triangulo);
+
+		System.out.println("Altura del triángulo: " + triangulo.getAltura());
+		System.out.println("Base del triángulo: " + triangulo.getBase());
+		System.out.println("Área del triángulo: " + areaTriangulo);
 	}
 
-	record Triangulo(String c, double b, double h) {}
+	public static double calcularAreaTriangulo(Triangulo triangulo) {
+		double base = triangulo.getBase();
+		double altura = triangulo.getAltura();
+		double area = 0.5 * base * altura;
+		return area;
+	}
+
+	record Triangulo(String color, double base, double altura) {
+		public double getBase() {
+			return base;
+		}
+
+		public double getAltura() {
+			return altura;
+		}
+	}
 }
--- src/main/java/org/example/ejercicios/Facturaciom3.java
+++ src/main/java/org/example/ejercicios/Facturaciom3.java
@@ -1,31 +1,35 @@
 package org.example.ejercicios;
-
 import java.util.List;
 
 public class Facturaciom3 {
-	record Factura(String cliente, String fechafactura, List<Double> precioProducto) {
+	record Invoice(String customer, String invoiceDate, List<Double> productPrice) {
 	}
+
+	private static final double INITIAL_VALUE = 0;
+	private static final String INVOICE_DATE_1 = "July 1, 1999";
+	private static final String INVOICE_DATE_2 = "August 15, 2000";
 
 	public static void main(String[] args) {
-		Factura factura1 = new Factura("Maria Sanabria", "Julio 1, 1999", List.of(4500D, 5000D, 2500D));
-		Factura factura2 = new Factura("Juan Perez", "Agosto 15, 2000", List.of(3500.0, 6000D, 2800D));
+		Invoice invoice1 = new Invoice("Maria Sanabria", INVOICE_DATE_1, List.of(4500D, 5000D, 2500D));
+		Invoice invoice2 = new Invoice("Juan Perez", INVOICE_DATE_2, List.of(3500.0, 6000D, 2800D));
 
-		double totalValorFact1 = 0;
-		for (Double precio : factura1.precioProducto) {
-			totalValorFact1 += precio;
-		}
-		System.out.println("Factura 1" );
-		System.out.println("Cliente: " + factura1.cliente());
-		System.out.println("Total: " + totalValorFact1);
+		double totalInvoiceAmount1 = calculateTotalInvoiceAmount(invoice1);
+		printInvoice(invoice1, totalInvoiceAmount1);
 
-
-		double totalValorFact2 = 0;
-		for (Double precio : factura2.precioProducto) {
-			totalValorFact2 += precio;
-		}
-		System.out.println("Factura 2" );
-		System.out.println("Cliente: " + factura2.cliente());
-		System.out.println("Total: " + totalValorFact2);
+		double totalInvoiceAmount2 = calculateTotalInvoiceAmount(invoice2);
+		printInvoice(invoice2, totalInvoiceAmount2);
 	}
 
+	private static double calculateTotalInvoiceAmount(Invoice invoice) {
+		return invoice.productPrice.stream()
+				.reduce(INITIAL_VALUE, Double::sum);
+	}
+
+	private static void printInvoice(Invoice invoice, double total) {
+		System.out.println("Invoice");
+		System.out.println("Customer: " + invoice.customer());
+		System.out.println("Date: " + invoice.invoiceDate());
+		System.out.println("Total: " + total);
+		System.out.println();
+	}
 }
